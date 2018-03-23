252a294c6f0c4a48dbca337baf272be06cf52519
Tibor V. Varga <tirgit@hotmail.com>
2018-03-23 09:59:05 +0100

    get_data function improvements

--- dimple_get_data.R
+++ dimple_get_data.R
@@ -22,30 +22,35 @@
 library(mice)
 library(VIM)
 library(Amelia)
+library(ggplot2)
+library(magrittr)
+library(dplyr)
 
 df <- data.frame(replicate(10,sample(0:1,1000,rep=TRUE)))
 library(missForest)
 df_miss <- prodNA(df, 0.3)
 
 dimple_get_data <- function(X, matrixplot_sort = F ,missplot = NA) {
+  comp <- sum(complete.cases(X))
   rows <- nrow(X)
   cols <- ncol(X)
   mat <- cor(X, use="complete.obs", method="pearson") 
-  missfrac <- sum(is.na(X))/(nrow(X)*ncol(X))
-  mdpat <- md.pattern(df_miss)
-  comp <- sum(complete.cases(df_miss))
-  list(Complete_cases = comp, Rows = rows, Columns = cols, Matrix = mat, Fraction_missingness = missfrac, MD_Pattern = mdpat)
+  missfrac_per_df <- sum(is.na(X))/(nrow(X)*ncol(X))
+  missfrac_per_var <- colMeans(is.na(df_miss))
+  na_per_df <-  sum(is.na(X))
+  na_per_var <- sapply(X, function(x) sum(length(which(is.na(x))))) 
+  mdpat <- md.pattern(X)
   
-  matrixplot(X, interactive = F)
   
+  nm1 <- names(X)[colSums(is.na(X)) >0]
+  arr_X <- X %>% 
+    arrange_at(vars(nm1), funs(desc(is.na(.))))
+  
+  if (matrixplot_sort == F) matrixplot(X, interactive = F, col= "red") else matrixplot(arr_X, interactive = F, col= "red")
+  list(Complete_cases = comp, Rows = rows, Columns = cols, Corr_matrix = mat, Fraction_missingness = missfrac_per_df, Fraction_missingness_per_variable = missfrac_per_var, Total_NA = na_per_df, NA_per_variable = na_per_var, MD_Pattern = mdpat)
 }
 
-dimple_get_data(df_miss)
 
 
-matrixplot(df_miss, interactive = F, sortby = "X1")
-missmap(df_miss, rank.order = F)
-
-
-
+y <- dimple_get_data(df_miss)
 
