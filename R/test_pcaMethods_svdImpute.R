a475908e4d262486d366528c5172cab0c1dd8508
Tibor V. Varga <tirgit@hotmail.com>
2018-09-12 12:14:47 +0200

    change max ncp in PCA functions

--- R/test_pcaMethods_svdImpute.R
+++ R/test_pcaMethods_svdImpute.R
@@ -28,47 +28,47 @@
 # FUNCTION
 
 test_pcaMethods_svdImpute <- function(X_hat, list) {
-    
+
     index <- lapply(list, is.na)
-    
+
     pcaMethods_svdImpute_imp <- function(X) {
-        ncomp <- missMDA::estim_ncpPCA(X)
-        if (ncomp$ncp > 0) 
-            ressvdImpute <- pcaMethods::pca(X, method = "svdImpute", center = FALSE, 
+        ncomp <- missMDA::estim_ncpPCA(X, ncp.max = ncol(X) - 2)
+        if (ncomp$ncp > 0)
+            ressvdImpute <- pcaMethods::pca(X, method = "svdImpute", center = FALSE,
                 nPcs = ncomp$ncp) else ressvdImpute <- pca(X, method = "svdImpute", center = FALSE, nPcs = 2)
         imp_matrix <- ressvdImpute@completeObs
-        
+
         list(Imputed = imp_matrix)
     }
-    
+
     print("pcaMethods svdImpute imputation - in progress")
     log_output <- utils::capture.output(results <- lapply(list, pcaMethods_svdImpute_imp))
-    
+
     # using NA index to identify the original values (later set to missing)
     orig_MCAR <- X_hat[index[[1]]]
     orig_MAR <- X_hat[index[[2]]]
     orig_MNAR <- X_hat[index[[3]]]
-    if (length(index) == 4) 
+    if (length(index) == 4)
         orig_MAP <- X_hat[index[[4]]]
-    
+
     # using NA index to identify the imputed values
     imp_MCAR <- results$MCAR_matrix$Imputed[index[[1]]]
     imp_MAR <- results$MAR_matrix$Imputed[index[[2]]]
     imp_MNAR <- results$MNAR_matrix$Imputed[index[[3]]]
-    if (length(index) == 4) 
+    if (length(index) == 4)
         imp_MAP <- results$MAP_matrix$Imputed[index[[4]]]
-    
+
     # RMSE
     rmse_MCAR <- sqrt(mean((orig_MCAR - imp_MCAR)^2))
     rmse_MAR <- sqrt(mean((orig_MAR - imp_MAR)^2))
     rmse_MNAR <- sqrt(mean((orig_MNAR - imp_MNAR)^2))
-    if (length(index) == 4) 
+    if (length(index) == 4)
         rmse_MAP <- sqrt(mean((orig_MAP - imp_MAP)^2))
-    
-    if (length(index) == 4) 
+
+    if (length(index) == 4)
         list(MCAR_RMSE = rmse_MCAR, MAR_RMSE = rmse_MAR, MNAR_RMSE = rmse_MNAR, MAP_RMSE = rmse_MAP) else list(MCAR_RMSE = rmse_MCAR, MAR_RMSE = rmse_MAR, MNAR_RMSE = rmse_MNAR)
-    
-    
+
+
 }
 
 # LAB res <- dimple_all_patterns(yy$Simulated_matrix,
